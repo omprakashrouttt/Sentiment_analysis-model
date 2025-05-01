dc5519fda5ba4a02897c63147587f3ebe571399f
omprakashrouttt <omprakashrouttt1@gmail.com>
2025-05-01 13:39:13 +0530

    Updated backend API URL to Render

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Function to analyze sentiment from text input
     async function analyzeTextSentiment(text) {
         try {
-            const response = await fetch("http://127.0.0.1:5000/predict", {
+            const response = await fetch("https://sentiment-analysis-model-unqh.onrender.com/predict", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ text: text })
@@ -51,7 +51,7 @@ document.addEventListener("DOMContentLoaded", function () {
         formData.append("file", file);
 
         try {
-            const response = await fetch("http://127.0.0.1:5000/upload", {
+            const response = await fetch("https://sentiment-analysis-model-unqh.onrender.com/upload", {
                 method: "POST",
                 body: formData
             });
